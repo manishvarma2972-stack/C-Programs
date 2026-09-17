e4b7debec49936fcad84693395cb85c30466ebe3
Manish Varma <manishvarma2972@gmail.com>
2026-09-17 12:33:18 +0530

    even_numbers_up_to_n

---
+++ evennumbersupton.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int count, counter;
+
+  printf("Up to which number you want to print even number?\n");
+  scanf("%i", &count);
+  counter = 0;
+  printf("The even numbers up to %i are %i", count, counter);
+  counter = counter + 2;
+  while (counter <= count)
+  {
+    printf(", %i", counter);
+    counter = counter + 2;
+  }
+  printf(".\n");
+
+  return 0;
+}
